1a67b285194ae6b90e591bec85bc6a29406b5c96
Nick Ray <nickrray@gmail.com>
2020-08-06 10:59:08 -0500

    :construction: Basic form setup

--- src/components/CharacterDisplay/Form/index.js
+++ src/components/CharacterDisplay/Form/index.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 export const Form = () => {
 
+  // 2 inputs: 1 name - 1 job --no handler
+  // 1 button: submit --needs handler
   return (
-    <h2>Form</h2>
+    <form>
+      <label htmlFor="name">Name</label>
+      <input id="name" type="text" />
+      <label htmlFor="job">Job</label>
+      <input id="job" type="text" />
+      <button type="submit">Add Character</button>
+    </form>
   )
 }
