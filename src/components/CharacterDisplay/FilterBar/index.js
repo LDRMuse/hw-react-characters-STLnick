d56ab82b923536e5eaef28b158f60fb448929823
Nick Ray <nickrray@gmail.com>
2020-08-06 10:38:19 -0500

    :sparkles: Create handler/state, pass to FilterBar and use them

--- src/components/CharacterDisplay/FilterBar/index.js
+++ src/components/CharacterDisplay/FilterBar/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export const FilterBar = () => {
+export const FilterBar = ({ handler, text }) => {
 
   return (
-    <h2>FilterBar</h2>
+    <div>
+      <label htmlFor="filter">Filter By Name</label>
+      <input onChange={handler} type="text" id="filter" value={text} />
+    </div>
   )
+}
+
+FilterBar.propTypes = {
+  handler: PropTypes.func,
+  text: PropTypes.string
 }
--- src/components/CharacterDisplay/index.js
+++ src/components/CharacterDisplay/index.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { CharacterTable } from './CharacterTable'
 import { FilterBar } from './FilterBar'
 import { Form } from './Form'
 
 export const CharacterDisplay = () => {
+  const [filterText, setFilterText] = useState('')
+
+  const handleFilterTextChange = (e) => {
+    const newFilterText = e.target.value
+    setFilterText(newFilterText)
+  }
 
   return (
     <div>
       <h2>CharacterDisplay</h2>
-      <FilterBar />
+      <FilterBar handler={handleFilterTextChange} text={filterText} />
       <CharacterTable />
       <Form />
     </div>
