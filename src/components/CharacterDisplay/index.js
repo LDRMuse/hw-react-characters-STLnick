32d0bebeb3ecabc68f8882c16598caae28eab235
Nick Ray <nickrray@gmail.com>
2020-08-06 11:15:35 -0500

    :fire: Shorten code
    shorten code

--- src/components/CharacterDisplay/index.js
+++ src/components/CharacterDisplay/index.js
@@ -12,8 +12,7 @@ export const CharacterDisplay = () => {
 
   useEffect(() => {
     (async () => {
-      const data = await api.index()
-      setCharacters(data)
+      setCharacters(await api.index())
     })()
   }, [])
 
@@ -22,24 +21,12 @@ export const CharacterDisplay = () => {
     setFilterText(newFilterText)
   }
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault()
-    const jobInput = e.target.querySelector('#job')
-    const nameInput = e.target.querySelector('#name')
-
-    setCharacters([...characters, { name: nameInput.value, job: jobInput.value }])
-
-    // Reset input fields
-    jobInput.value = ''
-    nameInput.value = ''
-  }
-
   return (
     <div>
       <h2>CharacterDisplay</h2>
       <FilterBar handler={handleFilterTextChange} text={filterText} />
       <CharacterTable charactersToDisplay={characters} />
-      <Form handler={handleFormSubmit} />
+      <Form />
     </div>
   )
 }
